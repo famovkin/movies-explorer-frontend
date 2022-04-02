0f37d57a7befc037a065367ce7bba2e892c67e68
Tolya Fam <famovkin@yandex.ru>
2022-04-02 03:16:48 +0300

    feat: создать хук для кастомной валидации

---
+++ src/hooks/useCustomValidation.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function useFormAndValidation() {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const validate = (name, value) => {
+    switch (name) {
+      case "film-query":
+        if (value.length === 0) {
+          setErrors({ ...errors, [name]: "Запрос не может быть пустым" });
+          setIsFormValid(false);
+        } else if (!new RegExp(/^[а-яА-ЯёЁa-zA-Z]+$/).test(value)) {
+          setErrors({ ...errors, [name]: "Нужно ввести ключевое слово" });
+          setIsFormValid(false);
+        } else {
+          setIsFormValid(true);
+          setErrors({});
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    validate(name, value);
+    setValues({ ...values, [name]: value });
+  };
+
+  return {
+    values,
+    setValues,
+    errors,
+    handleChange,
+    isFormValid,
+  };
+}
+
+export default useFormAndValidation;
