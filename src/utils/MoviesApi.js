eb7eb8fbc65f120d31f4be733de94671bf7d9718
Tolya Fam <famovkin@yandex.ru>
2022-04-02 03:10:23 +0300

    feat: создать утилиту MoviesApi

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,26 @@
+class MoviesApi {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkServerResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то пошло не так. Обратитесь к разработчику`);
+  }
+
+  getMovies() {
+    return fetch(this._baseUrl, {
+      headers: this._headers,
+    }).then((res) => this._checkServerResponse(res));
+  }
+}
+
+export const beatFilmApi = new MoviesApi({
+  baseUrl: "https://api.nomoreparties.co/beatfilm-movies",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
