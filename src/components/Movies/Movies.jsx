1e0de8deec04de19a026866944ce596f653fd432
Tolya Fam <famovkin@yandex.ru>
2022-04-02 03:33:33 +0300

    feat: получить карточки черезSearchForm, использовав хук валидации

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -7,70 +7,14 @@ import Button from "../Button/Button";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
 import savedPageContext from "../../context/saved-page-context";
+import Preloader from "../Preloader/Preloader";
 
 import "./Movies.css";
 
-const allMovies = [
-  {
-    id: 1,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 2,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 3,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 4,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 5,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 6,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 7,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    id: 8,
-    title: "В погоне за Бенкси",
-    duration: 27,
-    imageUrl:
-      "https://images.unsplash.com/photo-1647755370031-2bb9782f922a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
-  },
-];
-
 function Movies() {
-  const { setOnSavedPage } = useContext(savedPageContext);
+  const { onSavedPage, setOnSavedPage } = useContext(savedPageContext);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => setOnSavedPage(false), [setOnSavedPage]);
 
   return (
@@ -78,11 +22,17 @@ function Movies() {
       <Header />
       <Container>
         <section className="movies movies-page__movies" aria-label="Фильмы">
-          <SearchForm />
-          <MoviesCardList data={allMovies} onMainPage />
-          <div className="movies__footer">
+          <SearchForm
+            setMovies={setMovies}
+            setIsLoading={setIsLoading}
+          />
+          {isLoading ? <Preloader /> : <MoviesCardList data={movies} />}
+          {!isLoading && movies && (
+            <p className="movies__message">Ничего не найдено</p>
+          )}
+          {/* <div className="movies__footer">
             <Button className="button_type_more">Ещё</Button>
-          </div>
+          </div> */}
         </section>
       </Container>
       <Footer />
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
+import useCustomValidation from "../../hooks/useCustomValidation";
+import { beatFilmApi } from "../../utils/MoviesApi";
 
 import Icons from "../Icons";
 import "./SearchForm.css";
 
-function SearchForm() {
+function SearchForm({ setMovies, setIsLoading, onSavedPage }) {
+  const [errorText, setErrorText] = useState("");
+  const { values, setValues, errors, handleChange, isFormValid } =
+    useCustomValidation();
+
+  const filterMovies = async (searchQuery) => {
+    setIsLoading(true);
+    const movies = await beatFilmApi
+      .getMovies()
+      .then((data) => data)
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+
+    const filteredMovies = movies.filter((movie) =>
+      movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setMovies(filteredMovies);
+  };
+
+  const handleSubmitFrom = (e) => {
+    e.preventDefault();
+    if (values["film-query"] === undefined) {
+      setErrorText("Запрос не может быть пустым");
+      return;
+    }
+    if (isFormValid) {
+      console.log("Запрос на сервер");
+      filterMovies(values["film-query"]);
+    }
+    setErrorText(errors["film-query"]);
+  };
+
   return (
-    <form className="search-form app__search-form" name="search-movie">
+    <form
+      className="search-form app__search-form"
+      name="search-movie"
+      onSubmit={handleSubmitFrom}
+      noValidate
+    >
       <div className="search-form__string">
         <Icons.Search className="search-form__icon" />
         <input
           className="search-form__input"
+          name="film-query"
           placeholder="Фильм"
           type="text"
           required
-        ></input>
+          onChange={handleChange}
+          value={values["film-query"] || ""}
+          autocomplete="off"
+        />
         <Button
           className="button button_type_search button_type_blue"
           type="submit"
@@ -22,6 +64,7 @@ function SearchForm() {
           <Icons.Search />
         </Button>
       </div>
+      <span className="search-form__error">{errorText}</span>
       <label className="search-form__label" htmlFor="short-film">
         <input
           className="search-form__radio"
