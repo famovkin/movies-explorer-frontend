8a8b24b1156fdaef6a4035bdc7ed3d5e27c3087d
Tolya Fam <famovkin@yandex.ru>
2022-04-03 05:00:56 +0300

    feat: создать кастомный хук валидации форм

--- src/hooks/useCustomValidation.js
+++ src/hooks/useCustomValidation.js
@@ -1,22 +1,66 @@
 import { useState } from "react";
+import validator from "validator";
 
-export const useCustomValidation = () => {
+import { deleteKeyFromObj } from "../utils/deleteKeyFromObj";
+
+export const UseCustomValidation = () => {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
   const [isFormValid, setIsFormValid] = useState(false);
 
+  const setRequiredError = (name) => {
+    setErrors({ ...errors, [name]: "Это обязательное поле" });
+  };
+
   const validate = (name, value) => {
     switch (name) {
       case "film-query":
-        if (value.length === 0) {
+        if (value.length === 0 || value.length === undefined) {
           setErrors({ ...errors, [name]: "Запрос не может быть пустым" });
-          setIsFormValid(false);
         } else if (!new RegExp(/^[а-яА-ЯёЁa-zA-Z]+$/).test(value)) {
           setErrors({ ...errors, [name]: "Нужно ввести ключевое слово" });
-          setIsFormValid(false);
         } else {
-          setIsFormValid(true);
-          setErrors({});
+          const newErrors = deleteKeyFromObj(errors, [name]);
+          setErrors(newErrors);
+        }
+        break;
+      case "name":
+        if (value.length === 0) {
+          setRequiredError(name);
+        } else if (!new RegExp(/^[а-яА-ЯёЁa-zA-Z\s/-]+$/).test(value)) {
+          setErrors({
+            ...errors,
+            [name]: "Используйте буквы, дефис или пробел",
+          });
+        } else {
+          const newErrors = deleteKeyFromObj(errors, [name]);
+          setErrors(newErrors);
+        }
+        break;
+      case "email":
+        if (value.length === 0) {
+          setRequiredError(name);
+        } else if (!validator.isEmail(value)) {
+          setErrors({
+            ...errors,
+            [name]: "Некорректный адрес электронной почты",
+          });
+        } else {
+          const newObjErrors = deleteKeyFromObj(errors, [name]);
+          setErrors(newObjErrors);
+        }
+        break;
+      case "password":
+        if (value.length === 0) {
+          setRequiredError(name);
+        } else if (value.length < 8) {
+          setErrors({
+            ...errors,
+            [name]: "Минимальная длина пароля — 8 символов",
+          });
+        } else {
+          const newObj = deleteKeyFromObj(errors, [name]);
+          setErrors(newObj);
         }
         break;
       default:
@@ -26,15 +70,16 @@ export const useCustomValidation = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    validate(name, value);
     setValues({ ...values, [name]: value });
+    validate(name, value);
   };
 
   return {
     values,
-    setValues,
     errors,
     handleChange,
+    setValues,
     isFormValid,
+    setIsFormValid,
   };
-}
+};
