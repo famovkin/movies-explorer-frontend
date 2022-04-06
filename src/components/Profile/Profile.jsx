ee4e5f9c5ded77be62c55c7dd804eda82b5764a4
Tolya Fam <famovkin@yandex.ru>
2022-04-07 00:31:29 +0300

    fix: не удалять из localStorage данные о сохраненных фильмах

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -43,7 +43,6 @@ function Profile({ setIsLoggedIn, submitHandler, message, isLoading }) {
   const signOut = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("queryData");
-    localStorage.removeItem("savedMovies");
     setIsLoggedIn(false);
     history.push("/");
     setCurrentUser({
